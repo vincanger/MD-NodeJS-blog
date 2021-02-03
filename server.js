0a5081888e8ba4f69ad4ae2e7350212bd6844835
vincanger <vincanger@gmail.com>
2021-02-03 18:56:08 +0100

    remove render get request

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.get('/', (req, res) => {
-  res.render('index.html');
-})
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log('running on port', port);
